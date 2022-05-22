96eb97eea7c598d891fc4031a713a2eda92d8bdd
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-05-23 00:38:33 +0500

    Юнит-тестирование. Использование unittest и coverage. #2 и рефакторинг

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -9,11 +9,36 @@ class CalculatorTest(TestCase):
     def test_minus(self):
         self.assertEqual(calculator('3 - 1'), 2)
 
-    def test_nulti(self):
+    def test_multi(self):
         self.assertEqual(calculator('3 * 3'), 9)
 
     def test_divide(self):
         self.assertEqual(calculator('6 / 2'), 3.0)
+
+    def test_no_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('abracadabra')
+        self.assertEqual('Выражение должно содержать хотя бы один знак (+-/*)', e.exception.args[0])
+
+    def test_two_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+2+2')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_many_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+3*10')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_no_ints(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2.2+3.0')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_strings(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('a+b')
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
 
 
 if __name__ == '__main__':
